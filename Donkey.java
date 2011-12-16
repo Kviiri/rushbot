28f4954c38a7f445653b30db2f1be56075eca5d8
Kviiri <kalle.viiri@cs.helsinki.fi>
2011-12-16 22:10:49 +0200

    Floyd-Warshall etc

--- Donkey.java
+++ Donkey.java
@@ -7,21 +7,28 @@ import java.util.ArrayList;
  */
 public class Donkey {
     private ArrayList<Lion> expendables;
+    private ArrayList<Things.Enemy> charlies;
     
     
-    private int[][][][] dist;       //fromX fromY toX toY
+    private float[][][][] dist;       //fromX fromY toX toY
+    private boolean[][] map;        //true = passable, false = not passable
     public Donkey() {
         this.expendables = new ArrayList<Lion>();
+        this.charlies = new ArrayList<Things.Enemy>();
+        //TODO: Populate expendables
+        //TODO: Populate charlies
+        //TODO: Parse wallmap
+        dist = floydWarshall(2, 2);
         
     }
-    private int[][][][] floydWarshall(int xSize, int ySize) {
-        int[][][][] ret = new int[xSize][ySize][xSize][ySize];
+    private float[][][][] floydWarshall(int xSize, int ySize) {
+        float[][][][] ret = new float[xSize][ySize][xSize][ySize];
         for(int x1 = 0; x1 < xSize; x1++) {
             for(int x2 = 0; x2 < xSize; x2++) {
                 for(int y1 = 0; y1 < ySize; y1++) {
                     for(int y2 = 0; y2 < ySize; y2++) {
                         if(x1 == x2 && y1 == y2) ret[x1][y1][x2][y2] = 0;
-                        else ret[x1][y1][x2][y2] = Integer.MAX_VALUE;
+                        else ret[x1][y1][x2][y2] = Float.POSITIVE_INFINITY;
                     }
                 }
             }
@@ -32,13 +39,26 @@ public class Donkey {
                     for(int yAt = 0; yAt < ySize; yAt++) {
                         for(int xDest = 0; xDest < xSize; xDest++) {
                             for(int yDest = 0; yDest < ySize; yDest++) {
-                                //NOTE: Seinä- ja naapuri tarkistus tähän!
-                                
+                                //if blocked
+                                if(!map[xDest][yDest]) {
+                                    dist[xAt][yAt][xDest][yDest] = Float.POSITIVE_INFINITY;
+                                    continue;
+                                }
+                                //if neighbor, distance is 1
+                                else if(Math.abs(yAt - yDest) + Math.abs(xAt - xDest) == 1) {
+                                    dist[xAt][yAt][xDest][yDest] = 1;
+                                    continue;
+                                }
+                                else {
+                                    dist[xAt][yAt][xDest][yDest] = Math.min(dist[xAt][yAt][xDest][yDest],
+                                            dist[xAt][yAt][xBetween][yBetween] + dist[xBetween][yBetween][xDest][yDest]);
+                                }
                             }
                         }
                     }
                 }
             }
         }
+        return ret;
     }
 }
