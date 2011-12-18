1d01434689c4b7fc90c1a132ea60b77cd584ee50
Kviiri <kalle.viiri@cs.helsinki.fi>
2011-12-18 16:32:07 +0200

    more input parsing

--- Donkey.java
+++ Donkey.java
@@ -10,6 +10,7 @@ import java.util.Scanner;
 public class Donkey {
     private HashMap<String, Lion> expendables;
     private ArrayList<Things.Enemy> charlies;
+    private ArrayList<Things.Grenade> grenades;
     Scanner lue;
     
     private float[][][][] dist;       //fromX fromY toX toY
@@ -17,11 +18,13 @@ public class Donkey {
     private boolean team;           //true = A, false = B
     private int ourScore;
     private int theirScore;
+    private Things.Flag ourFlag;
+    private Things.EnemyFlag enemyFlag;
     public Donkey() {
         this.expendables = new HashMap<String, Lion>();
         this.charlies = new ArrayList<Things.Enemy>();
-        dist = floydWarshall(2, 2);
         lue = new Scanner(System.in);
+        dist = floydWarshall(2, 2);
         
     }
     private void parseInitialEngineOutput() {
@@ -39,6 +42,33 @@ public class Donkey {
         
     }
     private void parseTurnEngineOutPut() {
+        String[] points = lue.nextLine().split(" ");    //Ourpoints Enemypoints
+        ourScore = Integer.parseInt(points[0]);
+        theirScore = Integer.parseInt(points[1]);
+        String[] flag = lue.nextLine().split(" ");      //ourFlag.x ourFlag.y
+        ourFlag = new Things.Flag(Integer.parseInt(flag[1]),Integer.parseInt(flag[2]));
+        //Next up - our soldiers
+        String[] next = lue.nextLine().split(" ");
+        do {
+            if(!next[0].equalsIgnoreCase("Soldier")) break;
+            expendables.get(next[1]).soldier = new Things.Soldier(next[1], Integer.parseInt(next[2]), Integer.parseInt(next[3]),
+                    Integer.parseInt(next[4]), Boolean.parseBoolean(next[5]), next[6]);
+            next = lue.nextLine().split(" ");       //"Soldier" name x y cooldown alive flag
+        } while(next[0].equalsIgnoreCase("Soldier"));
+        //Grenades
+        do {
+            if(!next[0].equalsIgnoreCase("Grenade")) break;
+            grenades.add(new Things.Grenade(Integer.parseInt(next[1]), Integer.parseInt(next[2]), Integer.parseInt(next[3])));
+            next = lue.nextLine().split(" ");
+        } while(next[0].equals("Grenade"));
+        enemyFlag = new Things.EnemyFlag(Integer.parseInt(next[1]), Integer.parseInt(next[2]));
+        do {
+            next = lue.nextLine().split(" ");
+            if(next.length == 0) break;
+            charlies.add(new Things.Enemy(next[1], Integer.parseInt(next[2]), Integer.parseInt(next[3]),
+                    Boolean.parseBoolean(next[4]), next[5]));
+        }
+        
         
     }
     private float[][][][] floydWarshall(int xSize, int ySize) {
